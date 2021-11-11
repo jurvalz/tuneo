29583faa9233d77b35999e2349ea9eb154886cf0
wilmer <wilmerpillman@gmail.com>
2021-11-10 21:47:14 -0500

    se agrega modelo producto pedido

--- Cliente/Tuneo/Version 1.0/Codigo Fuente/tuneows/src/main/java/com/tuneo/ws/tuneows/model/ProductoPedido.java
+++ Cliente/Tuneo/Version 1.0/Codigo Fuente/tuneows/src/main/java/com/tuneo/ws/tuneows/model/ProductoPedido.java
@@ -2,5 +2,31 @@ package com.tuneo.ws.tuneows.model;
 
 public class ProductoPedido {
 
-    
+    private Integer coProducto;
+    private String nombre;
+    private String imagen;
+
+    public Integer getCoProducto() {
+        return coProducto;
+    }
+
+    public void setCoProducto(Integer coProducto) {
+        this.coProducto = coProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
 }
