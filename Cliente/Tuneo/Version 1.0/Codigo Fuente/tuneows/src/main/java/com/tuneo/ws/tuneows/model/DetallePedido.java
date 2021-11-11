0cc44f182b7a3e5efbc95f8d12063048690ca112
wilmer <wilmerpillman@gmail.com>
2021-11-10 21:39:04 -0500

    Se agrega modelo detalle pedido

--- Cliente/Tuneo/Version 1.0/Codigo Fuente/tuneows/src/main/java/com/tuneo/ws/tuneows/model/DetallePedido.java
+++ Cliente/Tuneo/Version 1.0/Codigo Fuente/tuneows/src/main/java/com/tuneo/ws/tuneows/model/DetallePedido.java
@@ -2,5 +2,31 @@ package com.tuneo.ws.tuneows.model;
 
 public class DetallePedido {
 
-    
+    private Integer co_detalle_pedido;
+    private Integer cantidad;
+    private ProductoPedido producto;
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Integer getCo_detalle_pedido() {
+        return co_detalle_pedido;
+    }
+
+    public void setCo_detalle_pedido(Integer co_detalle_pedido) {
+        this.co_detalle_pedido = co_detalle_pedido;
+    }
+
+    public ProductoPedido getProducto() {
+        return producto;
+    }
+
+    public void setProducto(ProductoPedido producto) {
+        this.producto = producto;
+    }
 }
