05bc5cc8d6f2a9d5643107d15b4fbb499e5902e1
wilmer <wilmerpillman@gmail.com>
2021-11-10 21:46:01 -0500

    se agrega modelo producto

--- Cliente/Tuneo/Version 1.0/Codigo Fuente/tuneows/src/main/java/com/tuneo/ws/tuneows/model/Producto.java
+++ Cliente/Tuneo/Version 1.0/Codigo Fuente/tuneows/src/main/java/com/tuneo/ws/tuneows/model/Producto.java
@@ -2,5 +2,396 @@ package com.tuneo.ws.tuneows.model;
 
 public class Producto {
 
+    private Integer coProducto;
+    private String tipo;
+    private String claseEdad;
+    private String claseUso;
+    private String claseForma;
+    private String claseTema;
+    private String claseCategoria;
+    private String nombre;
+    private Double puntuacion;
+    private Integer voto;
+    private String tipoCara;
+    private String subtipoCara;
+    private String valorCara;
+    private String tipoBorde;
+    private String valorBorde;
+    private String textoBorde;
+    private String textoCara;
+    private String material;
+    private String medidas;
+    private Double peso;
+    private Double precio;
+    private Double precioMin;
+    private Double precioMax;
+    private Double precioOferta;
+    private String imagenPatronGif;
+    private String imagenPatronJpg;
+    private String imagen;
+    private String imagen3d;
+    private Integer coPatron;
     
+    private Integer cp_x;
+    private Integer cp_y;
+    private Integer cp_z; 
+    private Integer ct_x;
+    private Integer ct_y; 
+    private Integer ct_z;
+    private Integer hl_x;
+    private Integer hl_y;
+    private Integer hl_z;
+    private Integer perspectiva;
+    private Integer width;
+    private Integer height; 
+    private String tiempoEntrega;
+    private String caracteristicas;
+    
+    
+    
+    public String getCaracteristicas() {
+		return caracteristicas;
+	}
+
+	public void setCaracteristicas(String caracteristicas) {
+		this.caracteristicas = caracteristicas;
+	}
+
+	public String getTiempoEntrega() {
+		return tiempoEntrega;
+	}
+
+	public void setTiempoEntrega(String tiempoEntrega) {
+		this.tiempoEntrega = tiempoEntrega;
+	}
+
+	public Integer getCp_x() {
+		return cp_x;
+	}
+
+	public void setCp_x(Integer cp_x) {
+		this.cp_x = cp_x;
+	}
+
+	public Integer getCp_y() {
+		return cp_y;
+	}
+
+	public void setCp_y(Integer cp_y) {
+		this.cp_y = cp_y;
+	}
+
+	public Integer getCp_z() {
+		return cp_z;
+	}
+
+	public void setCp_z(Integer cp_z) {
+		this.cp_z = cp_z;
+	}
+
+	public Integer getCt_x() {
+		return ct_x;
+	}
+
+	public void setCt_x(Integer ct_x) {
+		this.ct_x = ct_x;
+	}
+
+	public Integer getCt_y() {
+		return ct_y;
+	}
+
+	public void setCt_y(Integer ct_y) {
+		this.ct_y = ct_y;
+	}
+
+	public Integer getCt_z() {
+		return ct_z;
+	}
+
+	public void setCt_z(Integer ct_z) {
+		this.ct_z = ct_z;
+	}
+
+	public Integer getHl_x() {
+		return hl_x;
+	}
+
+	public void setHl_x(Integer hl_x) {
+		this.hl_x = hl_x;
+	}
+
+	public Integer getHl_y() {
+		return hl_y;
+	}
+
+	public void setHl_y(Integer hl_y) {
+		this.hl_y = hl_y;
+	}
+
+	public Integer getHl_z() {
+		return hl_z;
+	}
+
+	public void setHl_z(Integer hl_z) {
+		this.hl_z = hl_z;
+	}
+
+	public Integer getPerspectiva() {
+		return perspectiva;
+	}
+
+	public void setPerspectiva(Integer perspectiva) {
+		this.perspectiva = perspectiva;
+	}
+
+	public Integer getWidth() {
+		return width;
+	}
+
+	public void setWidth(Integer width) {
+		this.width = width;
+	}
+
+	public Integer getHeight() {
+		return height;
+	}
+
+	public void setHeight(Integer height) {
+		this.height = height;
+	}
+
+	public String getTextoBorde() {
+		return textoBorde;
+	}
+
+	public void setTextoBorde(String textoBorde) {
+		this.textoBorde = textoBorde;
+	}
+
+	public String getTextoCara() {
+		return textoCara;
+	}
+
+	public void setTextoCara(String textoCara) {
+		this.textoCara = textoCara;
+	}
+
+	public Integer getCoPatron() {
+		return coPatron;
+	}
+
+	public void setCoPatron(Integer coPatron) {
+		this.coPatron = coPatron;
+	}
+
+	public Double getPrecioOferta() {
+		return precioOferta;
+	}
+
+	public void setPrecioOferta(Double precioOferta) {
+		this.precioOferta = precioOferta;
+	}
+
+	public String getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+
+	public String getImagen3d() {
+		return imagen3d;
+	}
+
+	public void setImagen3d(String imagen3d) {
+		this.imagen3d = imagen3d;
+	}
+
+	public Double getPrecioMin() {
+		return precioMin;
+	}
+
+	public void setPrecioMin(Double precioMin) {
+		this.precioMin = precioMin;
+	}
+
+	public Double getPrecioMax() {
+		return precioMax;
+	}
+
+	public void setPrecioMax(Double precioMax) {
+		this.precioMax = precioMax;
+	}
+
+	public String getImagenPatronGif() {
+		return imagenPatronGif;
+	}
+
+	public void setImagenPatronGif(String imagenPatronGif) {
+		this.imagenPatronGif = imagenPatronGif;
+	}
+
+	public String getImagenPatronJpg() {
+		return imagenPatronJpg;
+	}
+
+	public void setImagenPatronJpg(String imagenPatronJpg) {
+		this.imagenPatronJpg = imagenPatronJpg;
+	}
+
+	public Integer getCoProducto() {
+        return coProducto;
+    }
+
+    public void setCoProducto(Integer coProducto) {
+        this.coProducto = coProducto;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getClaseEdad() {
+        return claseEdad;
+    }
+
+    public void setClaseEdad(String claseEdad) {
+        this.claseEdad = claseEdad;
+    }
+
+    public String getClaseUso() {
+        return claseUso;
+    }
+
+    public void setClaseUso(String claseUso) {
+        this.claseUso = claseUso;
+    }
+
+    public String getClaseForma() {
+        return claseForma;
+    }
+
+    public void setClaseForma(String claseForma) {
+        this.claseForma = claseForma;
+    }
+
+    public String getClaseTema() {
+        return claseTema;
+    }
+
+    public void setClaseTema(String claseTema) {
+        this.claseTema = claseTema;
+    }
+
+    public String getClaseCategoria() {
+        return claseCategoria;
+    }
+
+    public void setClaseCategoria(String claseCategoria) {
+        this.claseCategoria = claseCategoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
+
+    public Double getPuntuacion() {
+		return puntuacion;
+	}
+
+	public void setPuntuacion(Double puntuacion) {
+		this.puntuacion = puntuacion;
+	}
+
+	public Integer getVoto() {
+		return voto;
+	}
+
+	public void setVoto(Integer voto) {
+		this.voto = voto;
+	}
+
+	public String getTipoCara() {
+        return tipoCara;
+    }
+
+    public void setTipoCara(String tipoCara) {
+        this.tipoCara = tipoCara;
+    }
+
+    public String getSubtipoCara() {
+        return subtipoCara;
+    }
+
+    public void setSubtipoCara(String subtipoCara) {
+        this.subtipoCara = subtipoCara;
+    }
+
+    public String getValorCara() {
+        return valorCara;
+    }
+
+    public void setValorCara(String valorCara) {
+        this.valorCara = valorCara;
+    }
+
+    public String getTipoBorde() {
+        return tipoBorde;
+    }
+
+    public void setTipoBorde(String tipoBorde) {
+        this.tipoBorde = tipoBorde;
+    }
+
+    public String getValorBorde() {
+        return valorBorde;
+    }
+
+    public void setValorBorde(String valorBorde) {
+        this.valorBorde = valorBorde;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getMedidas() {
+        return medidas;
+    }
+
+    public void setMedidas(String medidas) {
+        this.medidas = medidas;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
 }
