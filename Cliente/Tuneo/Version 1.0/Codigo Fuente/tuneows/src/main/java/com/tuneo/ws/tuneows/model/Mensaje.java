56845d7d950d062eb68528b11fce76043e865e30
wilmer <wilmerpillman@gmail.com>
2021-11-10 21:44:50 -0500

    se garga modelo mensaje

--- Cliente/Tuneo/Version 1.0/Codigo Fuente/tuneows/src/main/java/com/tuneo/ws/tuneows/model/Mensaje.java
+++ Cliente/Tuneo/Version 1.0/Codigo Fuente/tuneows/src/main/java/com/tuneo/ws/tuneows/model/Mensaje.java
@@ -2,6 +2,41 @@ package com.tuneo.ws.tuneows.model;
 
 public class Mensaje {
 
-    
+    private Integer coMensaje;
+    private String asunto;
+    private String cuerpo;
+    private String fecha;
+
+    public Integer getCoMensaje() {
+        return coMensaje;
+    }
+
+    public void setCoMensaje(Integer coMensaje) {
+        this.coMensaje = coMensaje;
+    }
+
+    public String getAsunto() {
+        return asunto;
+    }
+
+    public void setAsunto(String asunto) {
+        this.asunto = asunto;
+    }
+
+    public String getCuerpo() {
+        return cuerpo;
+    }
+
+    public void setCuerpo(String cuerpo) {
+        this.cuerpo = cuerpo;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
 
 }
