1f1bf7d27707418c0350b0df3694918ce5e5cfb3
wilmer <wilmerpillman@gmail.com>
2021-11-10 21:43:12 -0500

    se agrega modelo favorito

--- Cliente/Tuneo/Version 1.0/Codigo Fuente/tuneows/src/main/java/com/tuneo/ws/tuneows/model/Favorito.java
+++ Cliente/Tuneo/Version 1.0/Codigo Fuente/tuneows/src/main/java/com/tuneo/ws/tuneows/model/Favorito.java
@@ -2,5 +2,31 @@ package com.tuneo.ws.tuneows.model;
 
 public class Favorito {
 
-    
+    private Integer coFavorito;
+    private Integer coProducto;
+    private String fecha;
+
+    public Integer getCoFavorito() {
+        return coFavorito;
+    }
+
+    public void setCoFavorito(Integer coFavorito) {
+        this.coFavorito = coFavorito;
+    }
+
+    public Integer getCoProducto() {
+        return coProducto;
+    }
+
+    public void setCoProducto(Integer coProducto) {
+        this.coProducto = coProducto;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
 }
