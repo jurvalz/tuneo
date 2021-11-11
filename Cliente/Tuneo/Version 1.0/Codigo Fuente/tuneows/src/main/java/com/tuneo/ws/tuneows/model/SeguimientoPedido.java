685bdfcad9f0bd35275d681ff127e480f6972353
wilmer <wilmerpillman@gmail.com>
2021-11-10 21:48:42 -0500

    se agrega modelo seguimiento pedido

--- Cliente/Tuneo/Version 1.0/Codigo Fuente/tuneows/src/main/java/com/tuneo/ws/tuneows/model/SeguimientoPedido.java
+++ Cliente/Tuneo/Version 1.0/Codigo Fuente/tuneows/src/main/java/com/tuneo/ws/tuneows/model/SeguimientoPedido.java
@@ -2,5 +2,40 @@ package com.tuneo.ws.tuneows.model;
 
 public class SeguimientoPedido {
 
-    
+    private Integer estado;
+    private Integer coPedido;
+    private String fecha;
+    private String tipo;
+
+    public Integer getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Integer estado) {
+        this.estado = estado;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Integer getCoPedido() {
+        return coPedido;
+    }
+
+    public void setCoPedido(Integer coPedido) {
+        this.coPedido = coPedido;
+    }
 }
